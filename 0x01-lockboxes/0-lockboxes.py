655ba977a07d75daba7a1f11eb7d5f3411b034c0
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-07-04 10:33:40 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,40 +1,20 @@
 #!/usr/bin/python3
+"""
+Method to determine if all boxes can be opened
+Using prototype: def canUnlockAll(boxes)
+"""
 
-from typing import List
 
-
-def canUnlockAll(boxes: List[List[int]]) -> bool:
+def canUnlockAll(boxes):
     """
-    Check if all the boxes can be opened.
-
-    Args:
-        boxes: A list of lists representing the boxes and their keys.
-
-    Returns:
-        True if all boxes can be opened, False otherwise.
+    Check if boxes can be unlocked
     """
-    n = len(boxes)
-    visited = [False] * n
-    visited[0] = True
-    queue = [0]
-
-    while queue:
-        box = queue.pop(0)
-        for key in boxes[box]:
-            if key < n and not visited[key]:
-                visited[key] = True
-                queue.append(key)
-
-    return all(visited)
-
-
-if __name__ == '__main__':
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))
-
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
