dd02a59b1fe5364695badfafe1907a42cf93d16d
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-07-26 18:51:37 +0100

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,7 @@ UTF-8 Validation
 """
 
 
-def valid_utf8(data):
+def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
 
@@ -43,11 +43,11 @@ def valid_utf8(data):
 
 if __name__ == "__main__":
     data = [65]
-    print(valid_utf8(data))
+    print(validUTF8(data))
 
     data = [80, 121, 116, 104, 111, 110, 32,
             105, 115, 32, 99, 111, 111, 108, 33]
-    print(valid_utf8(data))
+    print(validUTF8(data))
 
     data = [229, 65, 127, 256]
-    print(valid_utf8(data))
+    print(validUTF8(data))
